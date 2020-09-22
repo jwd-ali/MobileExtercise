44fb4ab4beee33ab4211dac2d76b03c30dae8978
jawad <jawad@digitify.com>
2020-09-23 00:07:08 +0500

    initial commit

---
+++ MobileExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileExercise
+//
+//  Created by Jawad Ali on 23/09/2020.
+//  Copyright © 2020 Jawad Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
